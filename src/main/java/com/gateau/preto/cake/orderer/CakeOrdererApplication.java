aa3d9f0391331c931bdbf346d2cda88d9e81ea75
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-04-21 22:27:05 -0300

    start project

---
+++ src/main/java/com/gateau/preto/cake/orderer/CakeOrdererApplication.java
@@ -0,0 +1,13 @@
+package com.gateau.preto.cake.orderer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeOrdererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeOrdererApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gateau/preto/cake/orderer/CakeOrdererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateau.preto.cake.orderer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeOrdererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
