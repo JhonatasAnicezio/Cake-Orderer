ecf41476c2f5de9a58b80b94cf8ab95b0b913250
Jhonatas Anicezio <98184336+JhonatasAnicezio@users.noreply.github.com>
2025-04-26 20:06:05 -0300

    Revert "Feature - 86a86h5n1: Implements coverage and checkstyle"

--- src/test/java/com/gateau/preto/cake/orderer/CakeOrdererApplicationTests.java
+++ src/test/java/com/gateau/preto/cake/orderer/CakeOrdererApplicationTests.java
@@ -7,8 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class CakeOrdererApplicationTests {
 
 	@Test
-	void main() {
-		CakeOrdererApplication.main(new String[] {});
+	void contextLoads() {
 	}
 
 }
