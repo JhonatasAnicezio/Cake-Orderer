522d8cbef170683a11a04eda8a16d15aa4166fe8
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-05-02 20:40:39 -0300

    feat: implements unit tests

---
+++ src/test/java/com/gateau/preto/cake/orderer/authentication/domain/UserTest.java
@@ -0,0 +1,28 @@
+package com.gateau.preto.cake.orderer.authentication.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+
+@SpringBootTest
+public class UserTest {
+
+  @Test
+  @DisplayName("Test methods getter")
+  public void testMethodGetPassword() {
+    SimpleGrantedAuthority authority = new SimpleGrantedAuthority("USER");
+    User user = User.builder()
+        .email("xicrinho@email.com")
+        .password("123456789")
+        .role("USER")
+        .build();;
+
+    Assertions.assertEquals("123456789", user.getPassword());
+    Assertions.assertEquals("xicrinho@email.com", user.getUsername());
+    Assertions.assertEquals(List.of(authority), user.getAuthorities());
+  }
+}
