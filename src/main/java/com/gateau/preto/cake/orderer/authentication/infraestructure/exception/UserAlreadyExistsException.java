a692d403e51c252f90d9e57276f1dbc034639171
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-05-06 23:34:39 -0300

    feat: create exception

--- src/main/java/com/gateau/preto/cake/orderer/authentication/infraestructure/exception/UserAlreadyExistsException.java
+++ src/main/java/com/gateau/preto/cake/orderer/authentication/infraestructure/exception/UserAlreadyExistsException.java
@@ -1,4 +1,6 @@
-package com.gateau.preto.cake.orderer.authentication.infraestructure;
+package com.gateau.preto.cake.orderer.authentication.infraestructure.exception;
+
+import com.gateau.preto.cake.orderer.core.exception.HandledException;
 
 public class UserAlreadyExistsException extends HandledException {
   public UserAlreadyExistsException() {
